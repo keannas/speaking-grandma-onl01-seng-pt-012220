855af3f04795cb854e7668de1c9d4cb7f0588c13
Keanna Salter <keannasalter91@gmail.com>
2020-01-27 22:38:50 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -4,5 +4,5 @@ if speak_to_gandma == "I MISS YOU!"
   puts "I LOVE YOU TOO PUMPKIN!"
 elsif speak_to_gandma == "Hi Nana, how are you?"
   puts "HUH?! SPEAK UP,SONNY!"
-
+elsif speak_to_gandma == ""
   
